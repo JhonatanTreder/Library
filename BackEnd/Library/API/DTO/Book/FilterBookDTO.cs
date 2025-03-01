477257cbec3d381bd15dd989f66ca104af759658
Jhonatan <jhonatan.treder@gmail.com>
2025-03-01 16:18:34 -0300

    Criando DTOs de livros para serem usados no repositorio

---
+++ BackEnd/Library/API/DTO/Book/FilterBookDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace API.DTO.Book
+{
+    public class FilterBookDTO
+    {
+        public string? Title { get; set; }
+        public string? Author { get; set; }
+        public string? Category { get; set; }
+        public string? Publisher { get; set; }
+        public int? PublicationYear { get; set; }
+    }
+}
