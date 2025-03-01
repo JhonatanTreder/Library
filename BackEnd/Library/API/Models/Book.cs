2e30342355c40db4ac87b17d60a66d80904bfca0
Jhonatan <jhonatan.treder@gmail.com>
2025-03-01 16:16:53 -0300

    Adicionando um novo atributo na entidade - Description

--- BackEnd/Library/API/Models/Book.cs
+++ BackEnd/Library/API/Models/Book.cs
@@ -19,12 +19,15 @@ namespace API.Models
         [StringLength(20, MinimumLength = 3, ErrorMessage = "A categoria deve conter entre 3 a 20 caracteres")]
         public string Category { get; set; } = string.Empty;
 
+        [StringLength(400, MinimumLength = 1, ErrorMessage = "A descrição do livro deve conter entre 1 a 400 caracteres")]
+        public string? Description { get; set; }
+
         [Required(ErrorMessage = "O nome da editora é obrigatória")]
         [StringLength(40, ErrorMessage = "O nome da editora deve conter no máximo 40 caracteres")]
         public string Publisher { get; set; } = string.Empty;
 
         [Range(1, int.MaxValue, ErrorMessage = "A quantidade deve ser pelo menos 1")]
-        public int Quantity { get; set; }
+        public int? Quantity { get; set; }
 
         [Range(1440, 2999, ErrorMessage = "O ano de publicação deve estar entre 1440 a 2999")]
         public int PublicationYear { get; set; }
