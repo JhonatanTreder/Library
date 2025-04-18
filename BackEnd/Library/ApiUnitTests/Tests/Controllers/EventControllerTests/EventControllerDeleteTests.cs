09a6f618a15ee3aed9ede65c3931762b9c2cb4cf
Jhonatan <jhonatan.treder@gmail.com>
2025-04-18 17:07:14 -0300

    test(EventController): criando testes unitários para o método HTTP DELETE de EventController

--- BackEnd/Library/ApiUnitTests/Tests/Controllers/EventControllerTests/EventControllerDeleteTests.cs
+++ BackEnd/Library/ApiUnitTests/Tests/Controllers/EventControllerTests/EventControllerDeleteTests.cs
@@ -1,6 +1,9 @@
 ﻿using API.Controllers;
+using API.DTO.Responses;
+using API.Enum.Responses;
 using API.Repositories.Interfaces;
 using ApiUnitTests.Fixtures;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -20,6 +23,35 @@ namespace ApiUnitTests.Tests.Controllers.EventControllerTests
         {
             _eventRepositoryMock = fixture.EventRepositoryMock;
             _controller = fixture.Controller;
+        }
+
+        [Fact]
+        public async Task DeleteEvent_ReturnNoContent()
+        {
+            int validId = 1;
+
+            _eventRepositoryMock.Setup(service => service.DeleteEventAsync(validId))
+                .ReturnsAsync(RepositoryStatus.Success);
+
+            var deleteResult = await _controller.Delete(validId);
+            var noContentResult = Assert.IsType<NoContentResult>(deleteResult);
+        }
+
+        [Fact]
+        public async Task DeleteEvent_ReturnNotFound()
+        {
+            int eventId = 1;
+
+            _eventRepositoryMock.Setup(service => service.DeleteEventAsync(eventId))
+                .ReturnsAsync(RepositoryStatus.NotFound);
+
+            var deleteResult = await _controller.Delete(eventId);
+            var notFoundResult = Assert.IsType<NotFoundObjectResult>(deleteResult);
+            var response = Assert.IsType<ApiResponse>(notFoundResult.Value);
+
+            Assert.Equal("Not Found", response.Status);
+            Assert.Null(response.Data);
+            Assert.Equal($"O evento de id '{eventId}' não foi encontrado", response.Message);
         }
     }
 }
