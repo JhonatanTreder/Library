a64fdefe6b1a7bd8275e401a337d33707cd04f5a
Jhonatan <jhonatan.treder@gmail.com>
2025-04-18 16:28:26 -0300

    test: criando configurações iniciais para realizar testes unitários em métodos HTTP DELETE de EventController

---
+++ BackEnd/Library/ApiUnitTests/Tests/Controllers/EventControllerTests/EventControllerDeleteTests.cs
@@ -0,0 +1,25 @@
+﻿using API.Controllers;
+using API.Repositories.Interfaces;
+using ApiUnitTests.Fixtures;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiUnitTests.Tests.Controllers.EventControllerTests
+{
+    [Collection("EventControllerCollection")]
+    public class EventControllerDeleteTests
+    {
+        private readonly Mock<IEventRepository> _eventRepositoryMock;
+        private readonly EventController _controller;
+
+        public EventControllerDeleteTests(EventControllerFixture fixture)
+        {
+            _eventRepositoryMock = fixture.EventRepositoryMock;
+            _controller = fixture.Controller;
+        }
+    }
+}
