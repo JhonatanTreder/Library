1beb6859052e8d7ca893249728f414f4d8e6f406
Jhonatan <jhonatan.treder@gmail.com>
2025-04-19 14:40:53 -0300

    test(EventController): configura a classe de testes unitários referente aos métodos HTTP GET do controlador EventController

---
+++ BackEnd/Library/ApiUnitTests/Tests/Controllers/EventControllerTests/EventControllerGetTests.cs
@@ -0,0 +1,26 @@
+﻿using API.Controllers;
+using API.Repositories.Interfaces;
+using ApiUnitTests.Fixtures;
+using Moq;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiUnitTests.Tests.Controllers.EventControllerTests
+{
+    [Collection("EventControllerCollection")]
+    public class EventControllerGetTests
+    {
+        private readonly Mock<IEventRepository> _eventRepositoryMock;
+        private readonly EventController _controller;
+
+        public EventControllerGetTests(EventControllerFixture fixture)
+        {
+            _eventRepositoryMock = fixture.EventRepositoryMock;
+            _controller = fixture.Controller;
+        }
+    }
+}
