3383bb46f6629c9c85f5a03c680b1cb6b736a5c2
Jhonatan <jhonatan.treder@gmail.com>
2025-04-19 16:02:47 -0300

    tes(EventController): implementa testes unitários referentes a métodos HTTP GET de EventController

--- BackEnd/Library/ApiUnitTests/Tests/Controllers/EventControllerTests/EventControllerGetTests.cs
+++ BackEnd/Library/ApiUnitTests/Tests/Controllers/EventControllerTests/EventControllerGetTests.cs
@@ -1,6 +1,11 @@
 ﻿using API.Controllers;
+using API.DTO.Event;
+using API.DTO.Responses;
+using API.Enum.Responses;
+using API.Models;
 using API.Repositories.Interfaces;
 using ApiUnitTests.Fixtures;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Newtonsoft.Json.Serialization;
 using System;
@@ -21,6 +26,107 @@ namespace ApiUnitTests.Tests.Controllers.EventControllerTests
         {
             _eventRepositoryMock = fixture.EventRepositoryMock;
             _controller = fixture.Controller;
+        }
+
+        [Fact]
+        public async Task GetEvents_ReturnOk()
+        {
+            var eventFilter = new EventFilterDTO 
+            {
+                Title = "valid-title",
+                TargetAudience = "valid-target-audience",
+                Location = "valid-location",
+                StartDate = DateTime.UtcNow,
+                EndDate = DateTime.UtcNow.AddHours(2)
+            };
+
+            var events = new List<Event> 
+            {
+                new Event(),
+                new Event(),
+                new Event()
+            };
+
+            _eventRepositoryMock.Setup(service => service.GetEventsAsync(eventFilter))
+                .ReturnsAsync(new RepositoryResponse<IEnumerable<Event>>(RepositoryStatus.Success, events));
+
+            var getResult = await _controller.Get(eventFilter);
+            var okSuccessResult = Assert.IsType<OkObjectResult>(getResult);
+            var response = Assert.IsType<ApiResponse>(okSuccessResult.Value);
+
+            Assert.Equal("Ok", response.Status);
+            Assert.NotNull(response.Data);
+            Assert.Equal("Eventos encontrados com sucesso", response.Message);
+        }
+
+        [Fact]
+        public async Task GetEvents_ReturnBadRequest_WhenEventIsNull()
+        {
+            var eventFilter = new EventFilterDTO();
+
+            _eventRepositoryMock.Setup(service => service.GetEventsAsync(eventFilter))
+                .ReturnsAsync(new RepositoryResponse<IEnumerable<Event>>(RepositoryStatus.NullObject));
+
+            var getResult = await _controller.Get(eventFilter);
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(getResult);
+            var response = Assert.IsType<ApiResponse>(badRequestResult.Value);
+
+            Assert.Equal("Bad Request", response.Status);
+            Assert.Null(response.Data);
+            Assert.Equal("O evento não pode ser nulo", response.Message);
+        }
+
+        [Fact]
+        public async Task GetEvents_ReturnNotFound_WhenEventsNotFound()
+        {
+            var eventFilter = new EventFilterDTO();
+
+            _eventRepositoryMock.Setup(service => service.GetEventsAsync(eventFilter))
+                .ReturnsAsync(new RepositoryResponse<IEnumerable<Event>>(RepositoryStatus.NotFound));
+
+            var getResult = await _controller.Get(eventFilter);
+            var notFoundResult = Assert.IsType<NotFoundObjectResult>(getResult);
+            var response = Assert.IsType<ApiResponse>(notFoundResult.Value);
+
+            Assert.Equal("Not Found", response.Status);
+            Assert.Null(response.Data);
+            Assert.Equal("Nenhum evento foi encontrado", response.Message);
+        }
+
+        [Fact]
+        public async Task GetEventById_ReturnOk()
+        {
+            int validId = 1;
+
+            var _event = new Event();
+
+            _eventRepositoryMock.Setup(service => service.GetEventByIdAsync(validId))
+                .ReturnsAsync(new RepositoryResponse<Event>(RepositoryStatus.Success, _event));
+
+            var getResult = await _controller.Get(validId);
+            var okSuccessResult = Assert.IsType<OkObjectResult>(getResult);
+            var response = Assert.IsType<ApiResponse>(okSuccessResult.Value);
+
+            Assert.Equal("Ok", response.Status);
+            Assert.NotNull(response.Data);
+            Assert.Equal($"Evento de id '{validId}' encontrado com sucesso", response.Message);
+        }
+
+        [Fact]
+        public async Task GetEventById_ReturnNotFound_WhenEventNotFound()
+        {
+            int eventId = 1;
+
+            _eventRepositoryMock.Setup(service => service.GetEventByIdAsync(eventId))
+                .ReturnsAsync(new RepositoryResponse<Event>(RepositoryStatus.NotFound));
+
+            var getResult = await _controller.Get(eventId);
+            var notFoundResult = Assert.IsType<NotFoundObjectResult>(getResult);
+            var response = Assert.IsType<ApiResponse>(notFoundResult.Value);
+
+            Assert.Equal("Not Found", response.Status);
+            Assert.NotNull(response.Data);
+            Assert.Equal($"O evento de id '{eventId}' não foi encontrado", response.Message);
         }
     }
 }
