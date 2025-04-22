8351c5a7438ba42161fb1c212ed4b8c4f212b106
Jhonatan <jhonatan.treder@gmail.com>
2025-04-22 16:08:20 -0300

    test(UserController): adiciona configurações iniciais para realizar testes unitários referentes ao controlador UserController

---
+++ BackEnd/Library/ApiUnitTests/Tests/Controllers/UserControllerTests/UserControllerPostTests.cs
@@ -0,0 +1,24 @@
+﻿using API.Controllers;
+using API.Repositories.Interfaces;
+using ApiUnitTests.Fixtures;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiUnitTests.Tests.Controllers.UserControllerTests
+{
+    [Collection("UserControllerCollection")]
+    public class UserControllerPostTests
+    {
+        private readonly Mock<IUserRepository> _userRepositoryMock;
+        private readonly UserController _controller;
+        public UserControllerPostTests(UserControllerFixture fixture)
+        {
+            _userRepositoryMock = fixture.UserRepositoryMock;
+            _controller = fixture.Controller;
+        }
+    }
+}
