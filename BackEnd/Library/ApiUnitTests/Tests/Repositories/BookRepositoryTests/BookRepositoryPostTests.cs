b4266f0f4065ab2fabcc60641bc99ea8796ecc38
Jhonatan <jhonatan.treder@gmail.com>
2025-04-25 23:32:20 -0300

    test: adiciona e configura classes para realizar testes de integração referente ao repositório BookRepository

---
+++ BackEnd/Library/ApiUnitTests/Tests/Repositories/BookRepositoryTests/BookRepositoryPostTests.cs
@@ -0,0 +1,20 @@
+﻿using ApiUnitTests.Fixtures.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiUnitTests.Tests.Repositories.BookRepositoryTests
+{
+    [Collection("DatabaseCollection")]
+    public class BookRepositoryPostTests
+    {
+        private readonly BookRepositoryFixture _fixture;
+
+        public BookRepositoryPostTests(BookRepositoryFixture fixture)
+        {
+            _fixture = fixture;
+        }
+    }
+}
