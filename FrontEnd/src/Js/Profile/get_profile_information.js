dc491752bcd5f17332e298556e1e6403011d4d74
Jhonatan <jhonatan.treder@gmail.com>
2025-08-14 19:39:49 -0300

    feat(validations-container): cria container para as validações pendentes do usuário ao realizar uma requisição

--- FrontEnd/src/Js/Profile/get_profile_information.js
+++ FrontEnd/src/Js/Profile/get_profile_information.js
@@ -2,38 +2,16 @@ document.addEventListener('DOMContentLoaded', async () => {
     try {
         const token = localStorage.getItem('token');
 
-        if (!token){
+        if (!token) {
             return window.location.href = '/src/Auth/login.html';
         }
 
-        const tokenData = parseJWT(token);
-        const userEmail = tokenData['email']
+        const getUserResponse = await getUserRequest(token);
 
-        const getUserURL = `https://localhost:7221/User/email/${encodeURIComponent(userEmail)}`;
+        if (getUserResponse.status === "Ok") {
 
-        const getUserRequest = await fetch(getUserURL, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`,
-            }
-        })
+            const spans = document.querySelectorAll('.info-content');
 
-        if (getUserRequest.status === 404) {
-            localStorage.removeItem('token');
-            localStorage.removeItem('refreshToken');
-            localStorage.removeItem('userEmail');
-
-            window.location.href = '/src/Auth/register.html';
-        }
-
-        if (getUserRequest.status === 401) {
-            window.location.href = '/src/Auth/register.html';
-        }
-
-        const getUserResponse = await getUserRequest.json();
-
-        if (getUserRequest.ok) {
             const userInfo = {
                 name: getUserResponse.data.name,
                 email: getUserResponse.data.email,
@@ -41,63 +19,20 @@ document.addEventListener('DOMContentLoaded', async () => {
                 userType: getUserResponse.data.userType,
             }
 
-            const spans = document.querySelectorAll('.info-content');
-
             spans[0].innerText = userInfo.name;
             spans[1].innerText = userInfo.email;
             spans[3].innerText = userInfo.phoneNumber;
             spans[4].innerText = userInfo.userType;
 
-            if (token) {
-                const payload = await parseJWT(token);
+            await getUserDashBoard(token);
+            await getPendingValidations(token);
 
-                const userId = payload['nameid'];
+            const saveButton = document.getElementById('save-button');
 
-                const saveButton = document.getElementById('save-button');
+            saveButton.addEventListener('click', async() => {
 
-                saveButton.addEventListener('click', async (e) => {
-
-                    const userUpdateDTO = {
-                        name: spans[0].innerText,
-                        email: spans[1].innerText,
-                        phoneNumber: spans[3].innerText,
-                    }
-
-                    const updateUserURL = `https://localhost:7221/User/${encodeURIComponent(userId)}`;
-
-                    const putUserInfoResponse = await fetch(updateUserURL, {
-                        method: 'PUT',
-                        headers: {
-                            'Content-Type': 'application/json',
-                            'Authorization': `Bearer ${token}`,
-                        },
-                        body: JSON.stringify(userUpdateDTO)
-                    })
-
-                    console.log(putUserInfoResponse)
-                })
-
-                const userDashboardURL = `https://localhost:7221/User/${encodeURIComponent(userId)}/user-dashboard`;
-
-                const getUserDashboardResponse = await fetch(userDashboardURL, {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${token}`,
-                    }
-                });
-
-                const dashboardResponseData = await getUserDashboardResponse.json();
-
-                const cardContents = document.querySelectorAll('.card-content');
-
-                cardContents[0].innerText = dashboardResponseData.data['totalLoans']
-                cardContents[1].innerText = dashboardResponseData.data['totalFines']
-                cardContents[2].innerText = dashboardResponseData.data['favoriteBooks'].value ?? 0
-                cardContents[3].innerText = dashboardResponseData.data['totalLoans'].value ?? 0
-                cardContents[4].innerText = dashboardResponseData.data['entryDate']
-                cardContents[5].innerText = dashboardResponseData.data['eventsHeld'].value ?? 0
-            }
+                await updateUserRequest(token);
+            })
         }
 
     } catch (e) {
@@ -118,3 +53,160 @@ function parseJWT(token) {
     return JSON.parse(jsonPayload);
 }
 
+async function getUserRequest(token) {
+
+    const tokenData = parseJWT(token);
+    const userEmail = tokenData['email']
+
+    const getUserURL = `https://localhost:7221/User/email/${encodeURIComponent(userEmail)}`;
+
+    const getUserRequest = await fetch(getUserURL, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+        }
+    })
+
+    if (getUserRequest.status === 404) {
+        localStorage.removeItem('token');
+        localStorage.removeItem('refreshToken');
+        localStorage.removeItem('userEmail');
+
+        window.location.href = '/src/Auth/register.html';
+    }
+
+    if (getUserRequest.status === 401) {
+        window.location.href = '/src/Auth/register.html';
+    }
+
+    return await getUserRequest.json();
+}
+
+async function updateUserRequest(token) {
+
+    const spans = document.querySelectorAll('.info-content');
+
+    const userName= spans[0].innerText;
+    const userEmail = spans[1].innerText;
+    const userPassword = spans[2].innerText;
+    const userPhoneNumber = spans[3].innerText;
+
+    const userUpdateDTO = {
+        name: userName,
+        email: userEmail,
+        phoneNumber: userPhoneNumber
+    }
+
+    const passwordRegex =  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W).{6,}$/
+
+    if (passwordRegex.test(userPassword) === true) {
+        userUpdateDTO.password = userPassword;
+    }
+
+    console.log(userUpdateDTO)
+
+    const payload = await parseJWT(token);
+    const userId = payload['nameid'];
+
+    const updateUserURL = `https://localhost:7221/User/${encodeURIComponent(userId)}`;
+
+    const putUserInfoResponse = await fetch(updateUserURL, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+        },
+        body: JSON.stringify(userUpdateDTO)
+    })
+
+    console.log(putUserInfoResponse)
+}
+
+async function getPendingValidations(token) {
+
+    const payload = await parseJWT(token);
+
+    const userId = payload['nameid'];
+
+    const pendingValidationsURL = `https://localhost:7221/User/${encodeURIComponent(userId)}/pending-validations`;
+
+    const getPendingValidationsResponse = await fetch(pendingValidationsURL, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+        }
+    })
+
+    const response = await getPendingValidationsResponse.json();
+
+    const pendingValidations = {
+        email: response.data.email,
+        phoneNumber: response.data.phoneNumber,
+    }
+
+    await showPendingValidations(pendingValidations);
+}
+async function showPendingValidations(pendingValidations) {
+    const verifications = document.getElementById('pending-verifications')
+
+    if (pendingValidations !== null) {
+        if (pendingValidations.email != null) {
+
+            const emailContent = document.createElement('div');
+
+            emailContent.classList.add('verify-item');
+            emailContent.innerHTML = `<i class="material-symbols-outlined">Email</i>
+                    <h5 class="info-title">Email:</h5>
+                    <span class="info-content">${pendingValidations.email}</span>
+                    <button class="verify-button">Verificar</button>`
+
+            verifications.appendChild(emailContent)
+        }
+
+        if (pendingValidations.phoneNumber != null) {
+
+            const phoneContent = document.createElement('div');
+
+            phoneContent.classList.add('verify-item');
+            phoneContent.innerHTML = `<i class="material-symbols-outlined">Phone</i>
+                    <h5 class="info-title">Número de telefone:</h5>
+                    <span class="info-content">${pendingValidations.phoneNumber}</span>
+                    <button class="verify-button">Verificar</button>`
+
+            verifications.appendChild(phoneContent)
+        }
+
+        console.log(verifications)
+    }
+}
+async function getUserDashBoard(token) {
+
+    const payload = await parseJWT(token);
+
+    const userId = payload['nameid'];
+
+    const userDashboardURL = `https://localhost:7221/User/${encodeURIComponent(userId)}/user-dashboard`;
+
+    const getUserDashboardResponse = await fetch(userDashboardURL, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+        }
+    });
+
+    const dashboardResponseData = await getUserDashboardResponse.json();
+
+    const cardContents = document.querySelectorAll('.card-content');
+
+    cardContents[0].innerText = dashboardResponseData.data['totalLoans']
+    cardContents[1].innerText = dashboardResponseData.data['totalFines']
+    cardContents[2].innerText = dashboardResponseData.data['favoriteBooks'].value ?? 0
+    cardContents[3].innerText = dashboardResponseData.data['totalLoans'].value ?? 0
+    cardContents[4].innerText = dashboardResponseData.data['entryDate']
+    cardContents[5].innerText = dashboardResponseData.data['eventsHeld'].value ?? 0
+
+    return dashboardResponseData;
+}
